5350d4fd1a752e6c3add39e6bb2ee0b07b5a3dfe
sw33tr <dobremihai@gmail.com>
2014-02-27 20:02:38 +0200

    adaugat judete RO

---
+++ i18n/states/RO.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Counties of Romania
+ *
+ * @author 		WooThemes
+ * @category 	i18n
+ * @package 	WooCommerce/i18n
+ * @version     2.1.2
+ */
+global $states;
+
+$states['RO'] = array(
+	'AB' => __( 'Alba', 'woocommerce' ),
+	'AR' => __( 'Arad', 'woocommerce' ),
+	'AG' => __( 'Arges', 'woocommerce' ),
+	'BC' => __( 'Bacau', 'woocommerce' ),
+	'BH' => __( 'Bihor', 'woocommerce' ),
+	'BN' => __( 'Bistrita-Nasaud', 'woocommerce' ),
+	'BT' => __( 'Botosani', 'woocommerce' ),
+	'BV' => __( 'Brasov', 'woocommerce' ),
+	'BR' => __( 'Braila', 'woocommerce' ),
+	'BZ' => __( 'Buzau', 'woocommerce' ),
+	'CL' => __( 'Calarasi', 'woocommerce' ),
+	'CS' => __( 'Caras Severin', 'woocommerce' ),
+	'CJ' => __( 'Cluj', 'woocommerce' ),
+	'CT' => __( 'Constanta', 'woocommerce' ),
+	'CV' => __( 'Covasna', 'woocommerce' ),
+	'DB' => __( 'Dambovita', 'woocommerce' ),
+	'DJ' => __( 'Dolj', 'woocommerce' ),
+	'GL' => __( 'Galati', 'woocommerce' ),
+	'GR' => __( 'Giurgiu', 'woocommerce' ),
+	'GJ' => __( 'Gorj', 'woocommerce' ),
+	'HR' => __( 'Harghita', 'woocommerce' ),
+	'HD' => __( 'Hunedoara', 'woocommerce' ),
+	'IL' => __( 'Ialomita', 'woocommerce' ),
+	'IS' => __( 'Iasi', 'woocommerce' ),
+	'IF' => __( 'Ilfov', 'woocommerce' ),
+	'MM' => __( 'Maramures', 'woocommerce' ),
+	'MH' => __( 'Mehedinti', 'woocommerce' ),
+	'MS' => __( 'Mures', 'woocommerce' ),
+	'NT' => __( 'Neamt', 'woocommerce' ),
+	'OT' => __( 'Olt', 'woocommerce' ),
+	'PH' => __( 'Prahova', 'woocommerce' ),
+	'SM' => __( 'Satu Mare', 'woocommerce' ),
+	'SJ' => __( 'Salaj', 'woocommerce' ),
+	'SB' => __( 'Sibiu', 'woocommerce' ),
+	'SV' => __( 'Suceava', 'woocommerce' ),
+	'TR' => __( 'Teleorman', 'woocommerce' ),
+	'TM' => __( 'Timisoara', 'woocommerce' ),
+	'VS' => __( 'Vaslui', 'woocommerce' ),
+	'VL' => __( 'Valcea', 'woocommerce' ),
+	'VN' => __( 'Vrancea', 'woocommerce' )
+);
